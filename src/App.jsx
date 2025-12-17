34737c923230133f81d6ddf7622a6fe7b64fde41
marioquinonesn20-collab <marioquinonesn20@gmail.com>
2025-12-16 23:18:11 -0600

    Add routing and layout components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+/* Layout */
+import Sidebar from "./layout/Sidebar";
+import Header from "./layout/Header";
+
+/* Pages */
+import Home from "./modules/Home";
+import Finxa from "./modules/Finxa";
+import AIConta from "./modules/AIConta";
+import FactuRamval from "./modules/FactuRamval";
+import Core from "./modules/Core";
+
 export default function App() {
   return (
     <div className="app-shell">
